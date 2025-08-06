1e3a5f46903c3fd2987389a1e0c7e42cef5ab79b
purveshganer <118466317+purveshganer@users.noreply.github.com>
2025-08-06 23:10:41 +0530

    Updating import script base file with reader function.

--- imdb/import_script_base_file.py
+++ imdb/import_script_base_file.py
@@ -26,6 +26,36 @@ Creating a base class which will be used to create import script.
 its work will be to create a session.
 
 """
+from datetime import datetime
+import csv
+from typing import Generator
+from imdb.models import TitleAkas, TitleBasics, TitleCrew, TitleEpisode, TitlePrincipals, TitleRatings, NameBasics
+
 class Importing:
     def __init__(self):
         pass
+    
+    def base_reader(self, file_path:str, offset:int = 0) -> Generator[dict, None, None]:
+
+        now = datetime.now()
+        readable_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        logging.info(f"Started data importing at {readable_time}")
+        logging.info(f"Reading file: {file_path.split('/')[-1]}")
+        
+        try:
+            assert offset >= 0, "Offset should not be negative number."
+        except AssertionError as e:
+            logging.exception(f"Assertion failed {e}")
+
+        with open(file_path, 'r', encoding='utf-8') as f:
+            reader = csv.DictReader(f, delimiter='\t')
+            for i, row in enumerate(reader):
+                if i < offset:
+                    continue
+                yield row
+        
+        logging.info(f"Finished reading file: {file_path.split('/')[-1]}")
+
+    def base_importer(self, record:dict, table_name:str):
+        num_of_lines = 0
