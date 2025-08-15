f81d6a75c1ddbca3f25aafb3eb4b2445b4ef639b
purveshganer <118466317+purveshganer@users.noreply.github.com>
2025-08-15 23:20:47 +0530

    Added user credential table and a login.html file.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    ROLE_CHOICES = [
+        ('user', 'User'),
+        ('manager', 'Manager'),
+        ('admin', 'Admin'),
+    ]
+
+    email = models.EmailField(unique=True, max_length=255, db_index=True)
+    username = models.CharField(max_length=150, unique=True, db_index=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+    is_active = models.BooleanField(default=True)
+    password = models.CharField(max_length=30, db_index=True)
+
